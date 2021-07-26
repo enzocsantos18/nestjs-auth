019de534bf18d2fe1026673284890e2a5f025c28
Enzo Carvalho <enzocsantos18@gmail.com>
2021-07-26 11:58:06 -0300

    feat: Implementação findOne User

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      id: 1,
+      username: 'enzocsantos',
+      password: '1234',
+    },
+    {
+      id: 2,
+      username: 'nicolas',
+      password: '1234',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
