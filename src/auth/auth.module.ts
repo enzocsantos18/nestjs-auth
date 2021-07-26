71ead7cdc672fc43949f986fc0710a0ba87e4659
Enzo Carvalho <enzocsantos18@gmail.com>
2021-07-26 13:42:17 -0300

    feat: Implementação LocalStrategy e rota de login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { UsersModule } from './../users/users.module';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService],
+  controllers: [AuthController],
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
