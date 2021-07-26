019357be9960c9dab89dc5bed620768a32738ad5
Enzo Carvalho <enzocsantos18@gmail.com>
2021-07-26 15:55:13 -0300

    fix: Liberando rota de login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,11 +2,13 @@ import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { AuthService } from './auth.service';
 import { Controller, Post, UseGuards, Request, Get } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { Public } from 'src/utils/Public';
 
 @Controller()
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
