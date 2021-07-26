518b0dd7d1ea57f24c9aa3bc2b82d2ab06c8c5fe
Enzo Carvalho <enzocsantos18@gmail.com>
2021-07-26 14:08:59 -0300

    feat: Criação do token JWT com payload do usuário

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,14 @@
+import { AuthService } from './auth.service';
 import { Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller()
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
